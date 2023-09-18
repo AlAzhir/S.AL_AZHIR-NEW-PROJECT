86d802daa3dc4ec9a8db245de7905b146f888e6a
AlAzhir <119715878+AlAzhir@users.noreply.github.com>
2023-09-18 00:49:22 -0700

    Add files via upload

---
+++ Lep year checker.py
@@ -0,0 +1,16 @@
+def is_leap_year(year):
+    if year%4==0:
+       if year%100==0:
+           if year%400==0:
+               print('Leap Year')
+           else:
+               print('Not leap year')
+       else:
+           print('Leap year')
+    else:
+       print('Not leap year')
+     
+print("Enter the year:")
+n=int(input(""))
+
+is_leap_year(n)                   
